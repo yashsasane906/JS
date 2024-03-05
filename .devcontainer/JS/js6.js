2596b5679382876af1e52d3bd1d99e144285866e
Yash Sasane <124269791+yashsasane906@users.noreply.github.com>
2024-03-05 11:57:18 +0000

    how memory works - stack , heap

--- .devcontainer/JS/js6.js
+++ .devcontainer/JS/js6.js
@@ -32,3 +32,36 @@ console.log(typeof myfunction); // function object
 
 
 console.log(typeof id); // sybmol 
+
+
+//===========================================//
+//=======HOW MEMORY WORKS====================//
+
+// 2 TYPES OF MEMORY =>  
+
+//+ stack are used in  (primitive data types)
+//+ heap are used in  ( non primitive data types)
+
+
+let myname = "yash"
+
+let anothername = myname
+anothername = "dishant"
+
+console.log(anothername);
+
+console.log(myname);
+
+
+let user1 ={
+    email: "yashsasane906@gmail.com",
+    upi: "user@bl"
+}
+
+let user2 = user1
+
+user2.email ="pradnyasasane@google.com"
+
+
+console.log(user1.email);
+console.log(user2.email);
