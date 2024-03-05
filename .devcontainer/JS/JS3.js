3288181d0303b4df93087db1aa5d128be57ce179
Yash Sasane <124269791+yashsasane906@users.noreply.github.com>
2024-03-05 09:19:48 +0000

    Data types

---
+++ .devcontainer/JS/JS3.js
@@ -0,0 +1,24 @@
+"use strict";  // treal all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser 
+
+
+let name = "YASH"
+let AGE =   21
+let isLoggedIn = false
+
+
+
+// number => 2 to power 53
+// bigint
+// string =>true/false
+// string => "" 
+// null => standalone value 
+// underfined => abhi value assign nhi huii hai
+
+// symbol =>  we will us eit in react for unique for a component 
+
+//object
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
