5139506bd5a3b73ea7e7009583054ad2a2ec0467
Yash Sasane <124269791+yashsasane906@users.noreply.github.com>
2024-03-05 05:13:00 +0000

    js file 2

---
+++ .devcontainer/JS/JS2.js
@@ -0,0 +1,18 @@
+const accountId = 144
+let accountEmail ="yashsasane906@gmail.com"
+var accountPassword = "12345"
+accountCity = "mumbai"
+let accountState ;
+
+accountEmail ="fewfwefwe"
+accountPassword="234244234"
+accountCity = "pune"
+// accountId = 2 // not allowed 
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountCity);
+console.log(accountPassword);
+
+ console.table([accountEmail,accountId,accountPassword,accountState]);
