ba6d1097a8acfabab15b5421488d2a0cf687f0dd
Yash Sasane <124269791+yashsasane906@users.noreply.github.com>
2024-03-05 10:02:40 +0000

    converting datatypes

---
+++ .devcontainer/JS/JS4.js
@@ -0,0 +1,27 @@
+let score = true
+
+// console.log(typeof 33); 
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber); 
+
+console.log( valueInNumber);  // ans = nan - not an number
+
+
+
+// "33"  =>33
+// "33abs" = Nan
+//  true = 1; false =>0
+
+let a = 1
+
+let xyz = Boolean(a)
+
+console.log(xyz);
+
+ let somenumber =33 
+ let stringnumber = String(somenumber)
+
+ console.log(typeof stringnumber);
