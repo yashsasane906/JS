79ea16bf6844f7eb5f7aa3e5145fabb2ec56e719
Yash Sasane <124269791+yashsasane906@users.noreply.github.com>
2024-03-05 10:29:06 +0000

    operations

---
+++ .devcontainer/JS/js5.js
@@ -0,0 +1,20 @@
+// let value = 3 
+// let negvalue = -value
+
+// console.log(negvalue);
+
+
+let str1 = "yash"
+let str2 = "xyz"
+
+let str3 =str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+
+console.log("1" + 2+2);
+
+console.log(1+2 +"2");
+
+console.log(+  true); 
